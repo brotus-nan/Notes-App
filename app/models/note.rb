035dff92ab33f7b10caa7333d5a2c6b4c2303174
brotus-nan <156174606+brotus-nan@users.noreply.github.com>
2024-01-10 15:26:11 +0200

    add note create/view

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,11 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+  validate :user_must_exist
   belongs_to :user
+
+  private
+  def user_must_exist
+    errors.add(:user, "must exist") unless User.exists?(self.user_id)
+  end
 end
